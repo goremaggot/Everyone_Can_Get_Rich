d707924167477ce10003ef4553516d3b5653bfaa
ELDUROA@gmail.com <Crfvtgb9>
2021-05-22 19:54:03 -0500

    Create Js , Simple Interest Calculator

---
+++ layout/scripts/calculator.js
@@ -0,0 +1,46 @@
+function calculator() {
+  var capital = document.getElementById('capital');
+  var interest = document.getElementById('interest');
+  var interestfloat;
+  var time = document.getElementById('time');
+  var typetime = document.getElementById('typetime');
+  var total;
+  var months;
+  var fieldempty = false;
+  var alertemptfields = "Please type the fields: "
+
+  if (capital.value == null || capital.value == 0) {
+    alertemptfields = alertemptfields + "\n" + "- Capital"
+    fieldempty = true;
+  }
+  if (interest.value == null || interest.value == 0) {
+    alertemptfields = alertemptfields + "\n" + "- Interest"
+    fieldempty = true;
+  }
+  if (time.value == null || time.value == 0) {
+    alertemptfields = alertemptfields + "\n" + "- Time"
+    fieldempty = true;
+  }
+  if (fieldempty) {
+    alert(alertemptfields);
+  }
+  else {
+    if (typetime.value == 1) {
+      months = time.value;
+      interestfloat = interest.value / 100;
+      total = capital.value * interestfloat * months;
+      alert("The result is $" + total )
+    }
+    else{
+      if (typetime.value == 2) {
+        months = time.value * 12;
+        interestfloat = interest.value / 100;
+        total = capital.value * interestfloat * months;
+        alert("The result is $" + total )
+      }
+      else {
+      alert("Error in Type of time")
+      }
+    }
+  }
+}
